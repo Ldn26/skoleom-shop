2f15717befa38a66ed8bd3f7a3a3ab5058a40e23
Ldn26 <youcefldn26@gmail.com>
2026-07-23 15:08:40 +0200

    allow c pg package

--- src/server/sequelize/config.js
+++ src/server/sequelize/config.js
@@ -1,6 +1,37 @@
 
 
+// const { Sequelize } = require("sequelize");
+// const dotenv = require("dotenv");
+
+// dotenv.config({ quiet: true });
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL is missing in .env");
+// }
+
+// const isProduction = process.env.NODE_ENV === "production";
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "postgres",
+//   logging: false,
+   
+//   dialectOptions: isProduction
+//     ? {
+//         ssl: {
+//           require: true,
+//           rejectUnauthorized: false,
+//         },
+//       }
+//     : {},
+// });
+
+// module.exports = sequelize;
+
+
+
+
 const { Sequelize } = require("sequelize");
+const pg = require("pg"); // 1. Import pg explicitly
 const dotenv = require("dotenv");
 
 dotenv.config({ quiet: true });
@@ -9,13 +40,12 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing in .env");
 }
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "development";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectModule: pg, // 2. Pass pg here to prevent dynamic lookup errors in Next.js/Turbopack
   logging: false,
-  // make it sync with heroku postgres ssl requirement
-   
   dialectOptions: isProduction
     ? {
         ssl: {
